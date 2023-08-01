b5d1242a666606bd358052960f43103b9d598a69
Alessandro Bianchi <alessandro.bianchi@db1.com.br>
2023-08-01 11:00:22 -0300

    feat:creating the account's API routes

---
+++ api/account.go
@@ -0,0 +1,147 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+	"time"
+
+	db "github.com/alessandrorbianchi/gofinance-backend/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountRequest struct {
+	UserID      int32     `json:"user_id" binding:"required"`
+	CategoryID  int32     `json:"category_id" binding:"required"`
+	Title       string    `json:"title" binding:"required"`
+	Type        string    `json:"type" binding:"required"`
+	Description string    `json:"description" binding:"required"`
+	Value       int32     `json:"value" binding:"required"`
+	Date        time.Time `json:"date" binding:"required"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	arg := db.CreateAccountParams{
+		UserID:      req.UserID,
+		CategoryID:  req.CategoryID,
+		Title:       req.Title,
+		Type:        req.Type,
+		Description: req.Description,
+		Value:       req.Value,
+		Date:        req.Date,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int32 `uri:"id" binding:"required"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type deleteAccountRequest struct {
+	ID int32 `uri:"id" binding:"required"`
+}
+
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, true)
+}
+
+type updateAccountRequest struct {
+	ID          int32  `json:"id" binding:"required"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Value       int32  `json:"value"`
+}
+
+func (server *Server) updateAccount(ctx *gin.Context) {
+	var req updateAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	arg := db.UpdateAccountParams{
+		ID:          req.ID,
+		Title:       req.Title,
+		Description: req.Description,
+		Value:       req.Value,
+	}
+
+	account, err := server.store.UpdateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountsRequest struct {
+	UserID      int32     `json:"user_id" binding:"required"`
+	Type        string    `json:"type" binding:"required"`
+	CategoryID  int32     `json:"category_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Date        time.Time `json:"date"`
+}
+
+func (server *Server) getAccounts(ctx *gin.Context) {
+	var req getAccountsRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	arg := db.GetAccountsParams{
+		UserID:      req.UserID,
+		CategoryID:  req.CategoryID,
+		Type:        req.Type,
+		Title:       req.Title,
+		Description: req.Description,
+		Date:        req.Date,
+	}
+
+	accounts, err := server.store.GetAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
