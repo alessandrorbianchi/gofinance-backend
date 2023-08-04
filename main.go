07492a6c1cb6d430d0e8464d4f766600ce19e695
Alessandro Bianchi <alessandro.bianchi@db1.com.br>
2023-08-04 09:59:38 -0300

    feat:Creating and using environment variables ( .env ) in the project

--- main.go
+++ main.go
@@ -3,19 +3,24 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	"github.com/alessandrorbianchi/gofinance-backend/api"
 	db "github.com/alessandrorbianchi/gofinance-backend/db/sqlc"
 	_ "github.com/lib/pq"
-)
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
-	serverAddress = "0.0.0.0:8000"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+	serverAddress := os.Getenv("SERVER_ADDRESS")
+
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
